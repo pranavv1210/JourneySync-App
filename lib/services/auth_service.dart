39561b9e6287876fce61206c568a74c120549d34
pranavv1210 <pranavv736@gmail.com>
2026-06-20 13:11:17 +0530

    fix: remove unnecessary url_launcher import from auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'app_config.dart';
 import 'supabase_service.dart';
