432920618e73b7deb1cc6005f1014c4bc0fbaccf
pranavv1210 <pranavv736@gmail.com>
2026-07-08 13:43:16 +0530

    Add Deno config for Supabase functions

--- supabase/functions/send-beta-welcome-email/index.ts
+++ supabase/functions/send-beta-welcome-email/index.ts
@@ -1,3 +1,5 @@
+/// <reference lib="deno.ns" />
+
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
